742c9a844b55c681f1eac98335292bebde05d0bc
Ramill <ramill_ibragimov@icloud.com>
2022-10-04 11:47:54 +0300

    Initial Commit

---
+++ ColorGradientAndShadowsInSwiftUI/ColorGradientAndShadowsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGradientAndShadowsInSwiftUIApp.swift
+//  ColorGradientAndShadowsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorGradientAndShadowsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
